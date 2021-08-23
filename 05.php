0e271be4563725513624127ece6c3f40b7a6519c
Gatis Ķevans <gatis.kevans@gmail.com>
2021-08-23 17:08:17 +0300

    Reshaped Code

--- 05.php
+++ 05.php
@@ -1,63 +1,66 @@
 <?php
 
-    class TicTacToe {
+class TicTacToe
+{
 
-        public array $gameCoordinates = [
-            [" ", " ", " "],
-            [" ", " ", " "],
-            [" ", " ", " "]
-        ];
+    public array $gameCoordinates = [
+        [" ", " ", " "],
+        [" ", " ", " "],
+        [" ", " ", " "]
+    ];
 
-        public int $emptyStrings = 0;
+    public int $emptyStrings;
 
-        function display_board()
-        {
-            echo " {$this->gameCoordinates[0][0]} | {$this->gameCoordinates[1][0]} | {$this->gameCoordinates[2][0]} \n";
-            echo "---+---+---\n";
-            echo " {$this->gameCoordinates[0][1]} | {$this->gameCoordinates[1][1]} | {$this->gameCoordinates[2][1]} \n";
-            echo "---+---+---\n";
-            echo " {$this->gameCoordinates[0][2]} | {$this->gameCoordinates[1][2]} | {$this->gameCoordinates[2][2]} \n";
-            echo "\n";
-        }
+    function display_board()
+    {
+        echo " {$this->gameCoordinates[0][0]} | {$this->gameCoordinates[0][1]} | {$this->gameCoordinates[0][2]} \n";
+        echo "---+---+---\n";
+        echo " {$this->gameCoordinates[1][0]} | {$this->gameCoordinates[1][1]} | {$this->gameCoordinates[1][2]} \n";
+        echo "---+---+---\n";
+        echo " {$this->gameCoordinates[2][0]} | {$this->gameCoordinates[2][1]} | {$this->gameCoordinates[2][2]} \n";
+        echo "\n";
+    }
 
-        function decideWhichPlayersTurn(): string {
-            $number = 0;
+    function playersTurn(): string
+    {
+        $number = 0;
 
-            for($i = 0; $i < count($this->gameCoordinates); $i++){
-                for($j = 0; $j < count($this->gameCoordinates[$i]); $j++){
-                    if($this->gameCoordinates[$i][$j] === " "){
-                        $number++;
-                        $this->emptyStrings = $number;
-                    } else {
-                        $this->emptyStrings = $number;
-                    }
+        for ($i = 0; $i < count($this->gameCoordinates); $i++) {
+            for ($j = 0; $j < count($this->gameCoordinates[$i]); $j++) {
+                if ($this->gameCoordinates[$i][$j] === " ") {
+                    $number++;
                 }
             }
-            if($number % 2 === 0){
-                return "O";
-            }
-            return "X";
         }
 
-        function makeAMove () {
-            $currentPlayer = $this->decideWhichPlayersTurn();
+        $this->emptyStrings = $number;
 
-            $row = readline("Enter row value: ");
-            $col = readline("Enter column value: ");
+        if ($number % 2 === 0) {
+            return "O";
+        }
+        return "X";
+    }
 
-            $isPromptActive = true;
-            while($isPromptActive) {
-                if(!is_numeric($row)){
-                    echo "Column $col\n";
-                    $row = readline("The value must be a number. Try again (row): ");
-                } else if(!is_numeric($col)){
-                    echo "Row $row\n";
-                    $col = readline("The value must be a number. Try again (column): ");
-                } else if($row > 2 || $row < 0){
-                    $row = readline("The value must be between 0-2. Try again (row): ");
-                } else if($col > 2 || $col < 0){
-                    $col = readline("The value must be between 0-2. Try again (column): ");
-                } else
+    function makeMove()
+    {
+        $currentPlayer = $this->playersTurn();
+
+        $row = readline("Enter row value: ");
+        $col = readline("Enter column value: ");
+
+        $isPromptActive = true;
+        while ($isPromptActive) {
+            if (!is_numeric($row)) {
+                echo "Column $col\n";
+                $row = readline("The value must be a number. Try again (row): ");
+            } else if (!is_numeric($col)) {
+                echo "Row $row\n";
+                $col = readline("The value must be a number. Try again (column): ");
+            } else if ($row > 2 || $row < 0) {
+                $row = readline("The value must be between 0-2. Try again (row): ");
+            } else if ($col > 2 || $col < 0) {
+                $col = readline("The value must be between 0-2. Try again (column): ");
+            } else
                 if ($this->gameCoordinates[$row][$col] !== " ") {
                     $row = readline("This area is already used. Try again (row value): ");
                     $col = readline("Enter column value:  ");
@@ -65,65 +68,57 @@
                     $isPromptActive = false;
                     $this->gameCoordinates[$row][$col] = $currentPlayer;
                 }
-            }
-
-        }
-
-        function findWinner(): string {
-            $coo = $this->gameCoordinates;
-            if($coo[0][0] === $coo[1][0] && $coo[1][0] === $coo[2][0] && $coo[0][0] !== " "){
-                return $coo[0][0];
-            }
-            if($coo[0][1] === $coo[1][1] && $coo[1][1] === $coo[2][1] && $coo[0][1] !== " "){
-                return $coo[0][1];
-            }
-            if($coo[0][2] === $coo[1][2] && $coo[1][2] === $coo[2][2] && $coo[0][2] !== " "){
-                return $coo[0][2];
-            }
-            if($coo[0][0] === $coo[0][1] && $coo[0][1] === $coo[0][2] && $coo[0][0] !== " "){
-                return $coo[0][0];
-            }
-            if($coo[1][0] === $coo[1][1] && $coo[1][1] === $coo[1][2] && $coo[1][0] !== " "){
-                return $coo[1][0];
-            }
-            if($coo[2][0] === $coo[2][1] && $coo[2][1] === $coo[2][2] && $coo[2][0] !== " "){
-                return $coo[2][0];
-            }
-            if($coo[0][0] === $coo[1][1] && $coo[1][1] === $coo[2][2] && $coo[0][0] !== " "){
-                return $coo[0][0];
-            }
-            if($coo[0][2] === $coo[1][1] && $coo[1][1] === $coo[2][0] && $coo[0][2] !== " "){
-                return $coo[0][2];
-            }
-            return " ";
-        }
-
-        function isTie(): bool {
-            return $this->emptyStrings === 0 && $this->findWinner() !== "X";
         }
 
     }
 
+    function findWinner(): string
+    {
+        $coo = $this->gameCoordinates;
+
+        for ($i = 0; $i <= 2; $i++) {
+            if ($coo[0][$i] === $coo[1][$i] && $coo[1][$i] === $coo[2][$i] && $coo[0][$i] !== " ") {
+                return $coo[0][$i];
+            }
+            if ($coo[$i][0] === $coo[$i][1] && $coo[$i][1] === $coo[$i][2] && $coo[$i][0] !== " ") {
+                return $coo[$i][0];
+            }
+        }
+
+        if ($coo[0][0] === $coo[1][1] && $coo[1][1] === $coo[2][2] && $coo[0][0] !== " ") {
+            return $coo[0][0];
+        }
+        if ($coo[0][2] === $coo[1][1] && $coo[1][1] === $coo[2][0] && $coo[0][2] !== " ") {
+            return $coo[0][2];
+        }
+        return " ";
+    }
+
+    function isTie(): bool
+    {
+        return $this->emptyStrings === 0 && $this->findWinner() !== "X";
+    }
+
+}
+
     $newGame = new TicTacToe();
 
-    function redrawBoard($newGame) {
-        $isInProgress = true;
-        while($isInProgress){
-            $currentPlayer = $newGame->decideWhichPlayersTurn();
+    function redrawBoard($newGame)
+    {
+        while (true) {
+            $currentPlayer = $newGame->playersTurn();
             echo "\nIt's $currentPlayer's turn:\n\n";
             $newGame->display_board();
-
-            if($newGame->isTie()){
+            if ($newGame->isTie()) {
                 echo "It's a tie!\n\n";
                 exit();
             }
+            $newGame->makeMove();
 
-            $newGame->makeAMove();
-
-            if($newGame->findWinner() !== " "){
-                echo "\nCongratulations $currentPlayer won!\n\n";
+            if ($newGame->findWinner() !== " ") {
+                echo "\nCongratulations {$newGame->findWinner()} won!\n\n";
                 $newGame->display_board();
-                $isInProgress = false;
+                exit();
             }
         }
     }
