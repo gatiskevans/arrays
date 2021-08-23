8bb7326a7c5183a50a9ba44384c4fda36ab2ec40
Gatis Ķevans <gatis.kevans@gmail.com>
2021-08-23 17:08:54 +0300

    Reshaped Code

--- 06.php
+++ 06.php
@@ -2,30 +2,30 @@
 
     class Hangman {
         public array $words = ['hangman', 'purgatory', 'magnetar', 'troposphere', 'programming'];
-        public array $wordIntoArray = [];
+        public array $chosenWord = [];
         public array $hiddenWord = [];
         public array $misses = [];
         public int $lives = 5;
 
         function chooseWord() {
-            $this->wordIntoArray = str_split($this->words[rand(0, count($this->words)-1)]);
+            $this->chosenWord = str_split($this->words[rand(0, count($this->words)-1)]);
         }
 
         function createHiddenField() {
             $emptyFields = [];
-            for($i = 0; $i < count($this->wordIntoArray); $i++){
+            for($i = 0; $i < count($this->chosenWord); $i++){
                 array_push($emptyFields, "_");
             }
             $this->hiddenWord = $emptyFields;
         }
 
         function checkChosenLetter(string $input){
-            foreach($this->wordIntoArray as $index => $letter){
+            foreach($this->chosenWord as $index => $letter){
                 if($letter === strtolower($input) && $this->hiddenWord[$index] === "_"){
                     $this->hiddenWord[$index] = $letter;
                 }
             }
-            if(!in_array(strtolower($input), $this->wordIntoArray)){
+            if(!in_array(strtolower($input), $this->chosenWord)){
                 if(!in_array(strtolower($input), $this->misses)){
                     array_push($this->misses, strtolower($input));
                     $this->lives--;
@@ -34,7 +34,7 @@
         }
 
         function win(): bool {
-            return $this->hiddenWord == $this->wordIntoArray;
+            return $this->hiddenWord == $this->chosenWord;
         }
 
         function lose(): bool {
@@ -53,9 +53,7 @@
                 if(strtoupper($option) === "N"){
                     die("Bye");
                 } else if(strtoupper($option) === "Y") {
-                    $this->wordIntoArray = [];
-                    $this->hiddenWord = [];
-                    $this->misses = [];
+                    $this->chosenWord = $this->hiddenWord = $this->misses = [];
                     $this->lives = 5;
                     $this->chooseWord();
                     $this->createHiddenField();
@@ -77,20 +75,23 @@
         echo "\nWord: " . implode(" ", $newGame->hiddenWord) . "\n\n";
         echo "Misses: " . implode("", $newGame->misses) . "\n\n";
 
+        if($newGame->win()){
+            echo "Congratulations! You have won\n";
+            $newGame->restartGame();
+            continue;
+        }
+        if($newGame->lose()){
+            echo "Game Over\n";
+            $newGame->restartGame();
+            continue;
+        }
+
         $input = readline("Guess: ");
         while(strlen($input) > 1 || is_numeric($input)){
             $input = readline("Invalid input. Try again: ");
         }
 
         $newGame->checkChosenLetter($input);
-        if($newGame->win()){
-            echo "Congratulations! You have won\n";
-            $newGame->restartGame();
-        }
-        if($newGame->lose()){
-            echo "Game Over\n";
-            $newGame->restartGame();
-        }
     }
 
 
